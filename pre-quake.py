d688b186ecde44e506603b8f3f13b789170cab8e
Shiraishi1996 <86176561+Shiraishi1996@users.noreply.github.com>
2022-12-01 13:22:22 +0900

    Update pre-quake.py

--- pre-quake.py
+++ pre-quake.py
@@ -51,7 +51,8 @@ plt.colorbar(plt.cm.ScalarMappable(norm, cmap))
 
 st.write("Googleにおける検索キーワードの都道府県毎の注目度")
 #plt.imshow(picture(dfjapan[search_keyword].apply(fcol)))
-st.image(picture(dfjapan[search_keyword].apply(fcol)))
+st.imshow(picture(dfjapan[search_keyword].apply(fcol)))
+st.pyplot(fig)
 
 st.write('【検索した単語】{}'.format(search_keyword))
 #検索順位取得処理
