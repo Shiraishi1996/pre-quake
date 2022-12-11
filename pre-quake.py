2b61fd31c7762c80247dc8213457ffab7a16ae47
Shiraishi1996 <86176561+Shiraishi1996@users.noreply.github.com>
2022-12-11 21:36:33 +0900

    Update pre-quake.py

--- pre-quake.py
+++ pre-quake.py
@@ -4948,8 +4948,8 @@ if colon:
         point = sorted(point)
         return pd.DataFrame({"time":point,"seismic intensity":point2.round().astype(int),"lat":point3,"lon":point4})
 
-    a = list_making()[list_making()["time"]<=date2]
-    a = a[a["time"]>=date]
+    a = list_making()[list_making()["time"]<=date2-1]
+    a = a[a["time"]>=date-1]
     
     st.title("地震予測AI分析")
     st.write("データが少なすぎる場合は、地震の予測結果がエラーを起こすことがあります。1年間の予測結果が閲覧できます。")
