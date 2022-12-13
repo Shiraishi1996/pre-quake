cb7aebc8ed897cdb7947fb9bfbc39b0e287472af
Shiraishi1996 <86176561+Shiraishi1996@users.noreply.github.com>
2022-12-14 08:55:52 +0900

    Update pre-quake.py

--- pre-quake.py
+++ pre-quake.py
@@ -6,6 +6,20 @@ import matplotlib.pyplot as mpl
 import numpy as np
 import statsmodels.api as sm
 import warnings
+import os
+
+# Everything is accessible via the st.secrets dict:
+
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
 
 st.image("pre-image.jpg", width = 100)
 st.title("地震予測シミュレーション")
